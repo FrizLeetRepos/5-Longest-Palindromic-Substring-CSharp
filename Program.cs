60ed4a78d42e2a23733e34ab8152f90550ce1317
frizzle101101 <aaronfritz96@gmail.com>
2024-11-04 15:21:08 -0500

    Commit Solution

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace _5_Longest_Palindromic_Substring {
+    internal class Program {
+        static void Main(string[] args) {
+            Solution solution = new Solution();
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public string LongestPalindrome(string s) {
+        //l = window length
+        //i = index through number of windows
+        var sarr = s.ToCharArray();
+        for (int l = s.Length; l >= 0; l--) {
+            for (int i = 0; i < s.Length - l + 1; i++) {
+                if (CheckForPalindrome(sarr, i, l)) {
+                    return s.Substring(i, l);
+                }
+            }
+        }
+        return string.Empty;
+    }
+
+    public bool CheckForPalindrome(char[] sarr, int index, int length) {
+        for (int i = 0; i < length; i++) {
+            if (sarr[index + i] != sarr[index + length - 1 - i])
+                return false;
+        }
+        return true;
+    }
+}
